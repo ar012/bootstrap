4a70bc43892a9f828dbe0dc637bdecf92ede01c3
Abdur Razzak <abdur.razzak@ctrends-software.com>
2019-03-04 14:39:23 +0600

    updated

---
+++ jquery/Ljquery.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('p').click(function(){
+    $(this).hide();
+  })
+})
+
+
+$(document).ready(function(){
+  $("button").click(function(){
+    $("h2").hide();
+  });
+});
